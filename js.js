26304c6f3da2e578ff8f26ebd3b3c7de44e1c403
Kovalev <Kovalev.K@citilink.ru>
2022-08-22 21:58:54 +0300

    init

--- js.js
+++ js.js
@@ -3,5 +3,7 @@ const button = document.querySelector('button');
 console.log(button)
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', () => {
-    alert('Этот сайт выполнен моими золотыми руками на HTML, CSS и JS 😎')
+    alert('Этот сайт выполнен моими золотыми руками на HTML, CSS и JS 😎' +
+        '\n' +
+        'P.s баги не гифка, я анимировала их сама 🪲❤️')
 })
